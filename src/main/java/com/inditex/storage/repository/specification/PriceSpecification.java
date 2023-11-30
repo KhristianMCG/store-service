3695cf58599ebbb756e316986673250db244f4de
Cristian Moreno <133767193+cristian-moreno-mimacom@users.noreply.github.com>
2023-11-30 02:11:46 +0100

    feature(core): IND - Include repository specification

--- src/main/java/com/inditex/storage/repository/specification/PriceSpecification.java
+++ src/main/java/com/inditex/storage/repository/specification/PriceSpecification.java
@@ -1,22 +1,45 @@
 package com.inditex.storage.repository.specification;
 
-import com.inditex.storage.api.SearchCriteria;
 import com.inditex.storage.model.PriceEntity;
+import com.inditex.storage.model.PriceEntity_;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
+import lombok.Setter;
+import om.inditex.storage.infra.rest.api.model.SearchCriteriaDto;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Setter
 public class PriceSpecification implements Specification<PriceEntity> {
 
-    private final SearchCriteria searchCriteria;
+    private transient SearchCriteriaDto searchCriteriaDto;
 
-    public PriceSpecification(final SearchCriteria searchCriteria) {
-        this.searchCriteria = searchCriteria;
-    }
     @Override
     public Predicate toPredicate(final Root<PriceEntity> root, final CriteriaQuery<?> query, final CriteriaBuilder criteriaBuilder) {
-        return null;
+        final List<Predicate> predicates = new ArrayList<>();
+
+        if (searchCriteriaDto.getBrandId() != null) {
+            predicates.add(criteriaBuilder.equal(root.get(PriceEntity_.BRAND), searchCriteriaDto.getBrandId()));
+        }
+        if (searchCriteriaDto.getProductId() != null) {
+            predicates.add(criteriaBuilder.equal(root.get(PriceEntity_.PRODUCT_ID), searchCriteriaDto.getProductId()));
+        }
+        if (searchCriteriaDto.getDateOfApplication() != null) {
+            predicates.add(criteriaBuilder.between(criteriaBuilder.literal(searchCriteriaDto.getDateOfApplication()),
+                    root.get(PriceEntity_.PRICE_START_DATE),
+                    root.get(PriceEntity_.PRICE_END_DATE)));
+        }
+        if (predicates.isEmpty()) {
+            predicates.add(criteriaBuilder.equal(root.get(PriceEntity_.ID), -1));
+        }
+
+        return query.where(criteriaBuilder.and(predicates.toArray(new Predicate[0])))
+                .distinct(true).orderBy(criteriaBuilder.desc(root.get(PriceEntity_.PRIORITY))).getRestriction();
     }
 }
--- src/main/java/com/inditex/storage/service/PriceService.java
+++ src/main/java/com/inditex/storage/service/PriceService.java
@@ -1,10 +1,10 @@
 package com.inditex.storage.service;
 
-import com.inditex.storage.api.SearchCriteria;
 import com.inditex.storage.converter.PriceEntityToDtoConverter;
 import com.inditex.storage.repository.PriceRepository;
 import com.inditex.storage.repository.specification.PriceSpecification;
 import om.inditex.storage.infra.rest.api.model.PriceDto;
+import om.inditex.storage.infra.rest.api.model.SearchCriteriaDto;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,13 +14,18 @@ public class PriceService {
 
     private final PriceRepository priceRepository;
     private final PriceEntityToDtoConverter priceEntityToDtoConverter;
-    public PriceService(final PriceRepository priceRepository, final PriceEntityToDtoConverter priceEntityToDtoConverter) {
+    private final PriceSpecification priceSpecification;
+
+    public PriceService(final PriceRepository priceRepository,
+                        final PriceEntityToDtoConverter priceEntityToDtoConverter,
+                        final PriceSpecification priceSpecification) {
         this.priceRepository = priceRepository;
         this.priceEntityToDtoConverter = priceEntityToDtoConverter;
+        this.priceSpecification = priceSpecification;
     }
 
-    public List<PriceDto> findAllFilteredPrices(final SearchCriteria searchCriteria) {
-        final PriceSpecification priceSpecification = new PriceSpecification(searchCriteria);
+    public List<PriceDto> findAllFilteredPrices(final SearchCriteriaDto searchCriteria) {
+        priceSpecification.setSearchCriteriaDto(searchCriteria);
         return priceEntityToDtoConverter.convertObjects(this.priceRepository.findAll(priceSpecification));
     }
 }
