0280e57cca36905517c821bb5f0eb6e84b3ddbbb
Cristian Moreno <133767193+cristian-moreno-mimacom@users.noreply.github.com>
2023-11-30 16:55:21 +0100

    feature(core): IND - Error message.

--- src/main/java/com/inditex/storage/exception/ErrorMessages.java
+++ src/main/java/com/inditex/storage/exception/ErrorMessages.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorMessages {
-    PRICES_NOT_FOUND("prices.notfound", "Any price for those dates could be found");
+    PRICES_NOT_FOUND("prices.notfound", "Any price for that date %s, brand %s and product %s could be found");
 
     private final String errorCode;
     private final String errorMessage;
